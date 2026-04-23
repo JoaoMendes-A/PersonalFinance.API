7fb9d5e8337cd4c535179dc8a44fdc24314bef9f
JoaoMendes-A <JoaoMenndesArj@hotmail.com>
2026-04-23 19:14:36 -0300

    feat(domain): add User entity for authentication

--- PersonalFinance.Api/src/PersonalFinance.Api/Models/User/User.cs
+++ PersonalFinance.Api/src/PersonalFinance.Api/Models/User/User.cs
@@ -10,8 +10,9 @@ public class User()
     [Key]
     public int Id { get; set; }
 
+    public string UserName { get; set; }
+
     public string Email { get; set; } = string.Empty;
 
-    public string Senha { get; set; } = string.Empty;
-
+    public string Password { get; set; } = string.Empty;
 };
