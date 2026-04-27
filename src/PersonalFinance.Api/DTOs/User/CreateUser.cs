8cc79a372ebfcd9ebe19c71f39a63b0e14a07bc8
JoaoMendes-A <JoaoMenndesArj@hotmail.com>
2026-04-27 17:59:32 -0300

    Feat: Cria DTO 'CreateUser'

---
+++ src/PersonalFinance.Api/DTOs/User/CreateUser.cs
@@ -0,0 +1,18 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalFinance.Api.DTOs.User;
+
+public class CreateUserDto{
+
+    [Required]
+    [StringLength(40, MinimumLength = 1)]
+    public string UserName { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(60, MinimumLength = 1)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string Password { get; set; } = string.Empty;
+}
